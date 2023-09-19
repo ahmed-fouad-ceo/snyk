9aac4983c6109c16d7229ddfeb1ad28e3bed3651
ahmed-fouad-ceo <75824466+ahmed-fouad-ceo@users.noreply.github.com>
2023-09-19 15:17:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 import re
 
-def match_regex(regex, string):
-    match = re.search(regex, string)
-    if match:
-        return match.group()
-    else:
-        return None
+def match_regex(regex, string, max_backtrack_steps=10000):
+    """Matches a regular expression against a string with a limited number of backtracking steps.
+
+    Args:
+        regex: The regular expression to match.
+        string: The string to match the regular expression against.
+        max_backtrack_steps: The maximum number of backtracking steps to perform.
+
+    Returns:
+        A match object if the regular expression matches the string, or None otherwise.
+    """
+
+    scanner = re.Scanner(regex)
+    match = None
+    while True:
+        match = scanner.search(string)
+        if not match or scanner.counters[0] > max_backtrack_steps:
+            break
+    return match
 
 if __name__ == "__main__":
     regex = input("Enter a regular expression: ")
